f1c4a1cd1dc92edf94e0d33a17ba938cd8490265
金秋 彭 <Jinqiu.Peng@WKEA.CN>
2021-05-12 16:05:30 +0800

    添加项目文件。

---
+++ pjqtest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace pjqtest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
